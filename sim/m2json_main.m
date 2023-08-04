209bccdc97427e393580553d9852c22ba2bc0f7a
nongfugengxia <nongfugengxia@163.com>
2023-08-04 16:25:44 +0800

    use submodule

--- sim/m2json_main.m
+++ sim/m2json_main.m
@@ -1,5 +1,9 @@
 
 
+% 需要递归的方式克隆整个项目 git clone <repository> --recursive
+% 更新子模块 git submodule update 
+addpath(fullfile(fileparts(pwd), 'utils', 'json'));
+
 m_path = 'data\test.m';
 
 s = m2json(m_path);
