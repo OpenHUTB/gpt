085c62c835ab9ef9c6bb6e04e6bf55f3a4435d4b
ggsdddup11 <229570098@qq.com>
2023-08-08 10:37:56 +0800

    改

--- sim/data/test_q5.m
+++ sim/data/test_q5.m
@@ -3,7 +3,7 @@ scenario = drivingScenario;
 roadCenters = [0 0; 30 0];
 road(scenario, roadCenters, 'Lanes', lanespec(3));
 
-v1 = vehicle(scenario, 'ClassID', 1,'Position',[0 4 0]);
+v1 = vehicle(scenario, 'ClassID', 1);
 v2 = vehicle(scenario, 'ClassID', 1);
 
 % 调整第二辆车的初始位置
--- sim/data/test_q6.m
+++ sim/data/test_q6.m
@@ -3,9 +3,9 @@ scenario = drivingScenario;
 roadCenters = [0 0; 30 0];
 road(scenario, roadCenters, 'Lanes', lanespec(3));
 
-v1 = vehicle(scenario, 'ClassID', 1,'Position',[0 4 0]);
-v2 = vehicle(scenario, 'ClassID', 2);
-v3 = vehicle(scenario, 'ClassID', 3,'Position',[0 -4 0]);
+v1 = vehicle(scenario, 'ClassID', 1);
+v2 = vehicle(scenario, 'ClassID', 1);
+v3 = vehicle(scenario, 'ClassID', 1);
 
 
 % 调整第二辆车的初始位置
