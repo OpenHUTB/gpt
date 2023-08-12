d600b732ea83675ecd9aa0b4f9afc2fdcea32cc5
xuece <xuece0312@163.com>
2023-08-12 18:56:50 +0800

    创建一个s弯，车辆等

---
+++ sim/data/x100.m
@@ -0,0 +1,19 @@
+% 创建一个有 S 形弯道的驾驶场景,中心点分别为(-30 20 0),(-20 -20 0),(0 0 0),(20 20 0),(35 -20 0)
+scenario = drivingScenario('StopTime',3);
+roadcenters = [-30 20 0; -20 -20 0; 0 0 0; 20 20 0; 35 -20 0];
+% 创建车道并将其添加到道路上
+lm = [laneMarking('Solid','Color','w'); ...
+    laneMarking('Dashed','Color','y'); ...
+    laneMarking('Dashed','Color','y'); ...
+    laneMarking('Solid','Color','w')];
+ls = lanespec(3,'Marking',lm);
+road(scenario,roadcenters,'Lanes',ls);
+% 添加一个自我车辆，并根据其航点指定其轨迹。默认情况下，汽车以每秒 30 米的速度行驶
+car = vehicle(scenario, ...
+    'ClassID',1, ...
+    'Position',[-35 20 0]);
+% 汽车坐标为(-31 20 0),(-20 -20 0),(0 0 0),(20 20 0),(35 -20 0)
+waypoints = [-31 20 0; -20 -20 0; 0 0 0; 20 20 0; 35 -20 0];
+smoothTrajectory(car,waypoints);
+
+plot(scenario)
---
+++ sim/data/x35.m
@@ -0,0 +1,12 @@
+% 为一条 80 米长的笔直道路创建一个驾驶场景和道路中心
+scenario = drivingScenario;
+roadCenters = [0 0; 80 0];
+% 为四车道道路创建 lanespec 对象。使用 laneMarking 函数指定其五个车道标记。中线为双实线和双黄线。最外侧的线为白色实线。内线为白色虚线。
+solidW = laneMarking('Solid','Width',0.3);
+dashW = laneMarking('Dashed','Space',5);
+doubleY = laneMarking('DoubleSolid','Color','yellow');
+lspec = lanespec([2 2],'Width',[5 5 5 5], ...
+    'Marking',[solidW dashW doubleY dashW solidW]);
+
+road(scenario,roadCenters,'Lanes',lspec);
+plot(scenario)
