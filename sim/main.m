75273b15db666092b97bd1f50e31fe05a51d682a
nongfugengxia <nongfugengxia@163.com>
2023-08-22 15:35:23 +0800

    setup(this) 方法

--- sim/main.m
+++ sim/main.m
@@ -5,6 +5,9 @@ function main(varargin)
 % 运行草地场景：main('D:\project\EmptyGrass4k4k\WindowsNoEditor\AutoVrtlEnv.exe')
 % 运行自定义场景：main('C:\Users\Administrator\Desktop\WorkPlace\DaoBao\WindowsNoEditor\XianJia.exe')
 
+% 常见问题：
+% 没有启动图形界面，只有数据发送：找不到exe文件
+
 % todo: 虚幻引擎启动时候过长，已经无响应了，需要杀死进程重新启动
 % 有时候启动没有道路
 % 去掉网页的两次点击
@@ -107,12 +110,11 @@ designer = driving.internal.scenarioApp.Designer(scenario);
 
 % 启动虚幻引擎场景查看器
 % 如果不出现，则调试 D:\work\matlab\toolbox\shared\drivingscenario\+driving\+scenario\+internal\GamingEngineScenarioAnimator.m
-% 中的 setupCommandReaderAndWriter()
+% 中的 setupCommandReaderAndWriter() 和 setup(this) 方法
 ue_viewer = designer.getGamingEngineViewer(1, 1);
-% ue_viewer.start();  % 开始仿真（失败）
+
 
 % 开始仿真（相当于点击驾驶场景设计器菜单中的运行）
-
 % sim_start_time = cputime
 % matlab\toolbox\shared\drivingscenario\+driving\+internal\+scenarioApp\Display.m
 % 有属性Simulator
