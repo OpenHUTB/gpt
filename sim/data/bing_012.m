85ac3cc0356547ddf45358dd5c23b30da93f9144
mghuna <437138609@qq.com>
2023-08-23 15:51:32 +0800

    直角转弯路段与共0-14辆轿车卡车

---
+++ sim/data/bing_012.m
@@ -0,0 +1,24 @@
+% 生成一个直角转弯路段,总共7辆轿车和卡车,轿车坐标是(-58.5,-2,0) (-22.2,1.9,0) ,卡车坐标是(-31.36,1.86,0) (-22.2,-1.9,0) (-13.7,1.7,0) (-13.7,-1.88,0) (-4.75,1.83,0) 
+scenario = drivingScenario;
+roadCenters = [0 0 0;
+    30 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [26.36 0.12 0;
+    26.36 -29.88 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road1');
+
+roadCenters = [-60 0 0;
+    1 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+%生成汽车
+vehicle(scenario,'ClassID', 1,'Position', [-58.5 -2 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-31.36 1.86 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-22.2 1.9 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-22.2 -1.9 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-13.7 1.7 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-13.7 -1.88 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-4.75 1.83 0]);
