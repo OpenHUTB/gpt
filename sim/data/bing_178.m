4d5fd64efac15c517ba39ebc67e64c114c2b2707
mghuna <437138609@qq.com>
2023-08-23 16:04:39 +0800

    一段两连弯的连续弯路与共0-14辆的轿车卡车

---
+++ sim/data/bing_178.m
@@ -0,0 +1,26 @@
+% 生成一段两连弯的连续弯路,总共9辆轿车和卡车,轿车坐标是(-49.5,-2.03,0) (-40.5,1.87,0) (-40.5,-1.96,0) (-31.36,1.86,0) (-22.2,-1.9,0) ,卡车坐标是(-49.5,1.97,0) (-31.36,-1.89,0) (-13.7,-1.88,0) (-4.75,1.83,0) 
+scenario = drivingScenario;
+roadCenters = [-3.1086244689504e-15 7.7715611723761e-16 0;
+    12 -0.3 0;
+    34.7 6.6 0;
+    37.7 33 0;
+    61.3 43.9 0;
+    84.5 43.1 0;
+    94.8 43.1 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [-60 0 0;
+    1 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+%生成汽车
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-49.5 1.97 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-49.5 -2.03 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-40.5 1.87 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-40.5 -1.96 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-31.36 1.86 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-31.36 -1.89 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-22.2 -1.9 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-13.7 -1.88 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-4.75 1.83 0]);
