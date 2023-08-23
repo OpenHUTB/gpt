38edf8d8540793573d8c4fde36cdf0c12ed945bd
mghuna <437138609@qq.com>
2023-08-23 16:13:21 +0800

    有缓弯路段的高速公路与共0-14辆的轿车卡车

---
+++ sim/data/bing_459.m
@@ -0,0 +1,33 @@
+% 生成一个有缓弯路段的高速公路,总共5辆轿车和卡车,轿车坐标是(-40.5,1.87,0) (-13.7,1.7,0) ,卡车坐标是(-58.5,2.1,0) (-49.5,1.97,0) (-4.75,1.83,0) 
+scenario = drivingScenario;
+roadCenters = [-2.9 0.1 0;
+    6 0 0;
+    23.6 -2.8 0;
+    38.4 -8.8 0;
+    45.8 -13.5 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [-60 0 0;
+    1 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+barrierCenters = [-60 0 0;
+    -2.9 0.1 0;
+    6 0 0;
+    23.6 -2.8 0;
+    38.4 -8.8 0;
+    45.8 -13.5 0];
+barrier(scenario, barrierCenters, ...
+    'ClassID', 5, ...
+    'Width', 0.61, ...
+    'Height', 0.81, ...
+    'Mesh', driving.scenario.jerseyBarrierMesh, 'PlotColor', [0.65 0.65 0.65], 'Name', 'Jersey Barrier');
+
+%生成汽车
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-58.5 2.1 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-49.5 1.97 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-40.5 1.87 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-13.7 1.7 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-4.75 1.83 0]);
