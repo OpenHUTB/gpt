ee7236c7edc25141f75354c36c6e28a8225f40d1
mghuna <437138609@qq.com>
2023-08-23 16:08:35 +0800

    一段双车道与共0-14辆的轿车卡车

---
+++ sim/data/bing_297.m
@@ -0,0 +1,22 @@
+% 生成一段双车道,总共10辆轿车和卡车,轿车坐标是(-58.5,2.1,0) (-40.5,-1.96,0) (-22.2,1.9,0) (-13.7,1.7,0) (-13.7,-1.88,0) (-4.75,1.83,0) ,卡车坐标是(-58.5,-2,0) (-49.5,-2.03,0) (-31.36,1.86,0) (-31.36,-1.89,0) 
+scenario = drivingScenario;
+roadCenters = [0 0 0;
+    30 0 0];
+laneSpecification = lanespec([2 2]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [-60 0 0;
+    1 0 0];
+laneSpecification = lanespec([2 2]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+%生成汽车
+vehicle(scenario,'ClassID', 1,'Position', [-58.5 2.1 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-58.5 -2 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-49.5 -2.03 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-40.5 -1.96 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-31.36 1.86 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-31.36 -1.89 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-22.2 1.9 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-13.7 1.7 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-13.7 -1.88 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-4.75 1.83 0]);
