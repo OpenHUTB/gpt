4c179435931a1c862a7426e37c005cfc14f260d2
mghuna <437138609@qq.com>
2023-08-23 16:06:00 +0800

    一段二合一的路与共0-14辆的轿车卡车

---
+++ sim/data/bing_209.m
@@ -0,0 +1,23 @@
+% 生成一段二合一的路,总共6辆轿车和卡车,轿车坐标是(-58.5,-2,0) (-49.5,-2.03,0) (-40.5,1.87,0) (-13.7,1.7,0) ,卡车坐标是(-31.36,-1.89,0) (-4.75,1.83,0) 
+scenario = drivingScenario;
+roadCenters = [36.8 0 0;
+    2.1 -15.3 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [0 0 0;
+    60 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [-60 0 0;
+    1 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+%生成汽车
+vehicle(scenario,'ClassID', 1,'Position', [-58.5 -2 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-49.5 -2.03 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-40.5 1.87 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-31.36 -1.89 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-13.7 1.7 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-4.75 1.83 0]);
---
+++ sim/data/bing_215.m
@@ -0,0 +1,24 @@
+% 生成一段二合一的路,总共7辆轿车和卡车,轿车坐标是(-58.5,2.1,0) (-31.36,-1.89,0) (-22.2,-1.9,0) (-13.7,1.7,0) (-13.7,-1.88,0) ,卡车坐标是(-58.5,-2,0) (-4.75,-1.85,0) 
+scenario = drivingScenario;
+roadCenters = [36.8 0 0;
+    2.1 -15.3 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [0 0 0;
+    60 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [-60 0 0;
+    1 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+%生成汽车
+vehicle(scenario,'ClassID', 1,'Position', [-58.5 2.1 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-58.5 -2 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-31.36 -1.89 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-22.2 -1.9 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-13.7 1.7 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-13.7 -1.88 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-4.75 -1.85 0]);
