0f284e0f6f7c2cae42e9985fefaea10e26c5bf27
mghuna <437138609@qq.com>
2023-08-23 15:56:58 +0800

    十字路口与0-14辆的轿车卡车

---
+++ sim/data/bing_049.m
@@ -0,0 +1,25 @@
+% 生成一个十字路口,总共8辆轿车和卡车,轿车坐标是(-49.5,1.97,0) (-49.5,-2.03,0) (-22.2,1.9,0) (-13.7,-1.88,0) ,卡车坐标是(-58.5,2.1,0) (-40.5,-1.96,0) (-13.7,1.7,0) (-4.75,-1.85,0) 
+scenario = drivingScenario;
+roadCenters = [0 0 0;
+    30 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+
+roadCenters = [15.06 15.25 0;
+    15.06 -14.75 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road1');
+
+roadCenters = [-60 0 0;
+    1 0 0];
+laneSpecification = lanespec([1 1]);
+road(scenario, roadCenters, 'Lanes', laneSpecification, 'Name', 'Road');
+%生成汽车
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-58.5 2.1 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-49.5 1.97 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-49.5 -2.03 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-40.5 -1.96 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-22.2 1.9 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-13.7 1.7 0]);
+vehicle(scenario,'ClassID', 1,'Position', [-13.7 -1.88 0]);
+vehicle(scenario,'ClassID', 2,'Height', 3.5,'Position', [-4.75 -1.85 0]);
