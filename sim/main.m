5516c5fc6bcef2a4775cb38ceb019fabffaa74c9
nongfugengxia <nongfugengxia@163.com>
2023-09-13 11:15:27 +0800

    修复源代码不在当前路径的问题

--- sim/main.m
+++ sim/main.m
@@ -5,14 +5,14 @@ function main(varargin)
 % 运行草地场景：main('D:\project\EmptyGrass4k4k\WindowsNoEditor\AutoVrtlEnv.exe')
 % 运行自定义场景：main('C:\Users\Administrator\Desktop\WorkPlace\DaoBao\WindowsNoEditor\XianJia.exe')
 
+src_code_path = fullfile(fileparts(mfilename('fullpath')), 'code.m');
+run(src_code_path);
 
-run('./code.m');
 if isempty(varargin)
     run_scenario(scenario);
 else
     run_scenario(scenario, varargin);
 end
-
 
 end
 
