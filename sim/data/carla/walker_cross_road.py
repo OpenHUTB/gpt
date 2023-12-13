853e22e5d2d66b0e2368a79b7fafbd7a3eefdebc
jiandaoshou-aidehua <2893666867@qq.com>
2023-12-13 16:20:29 +0800

    walker and vehicle code

--- sim/data/carla/walker_cross_road.py
+++ sim/data/carla/walker_cross_road.py
@@ -1,18 +1,12 @@
 
-#!/usr/bin/env python
-
-# Copyright (c) 2021 Computer Vision Center (CVC) at the Universitat Autonoma de
-# Barcelona (UAB).
-#
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
-
 """闯红灯时其他车辆等待场景"""
-
 import glob
 import os
 import random
 import sys
+import math
+import carla
+import argparse
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -21,27 +15,19 @@ try:
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
 except IndexError:
     pass
-import math
-import carla
-import argparse
-import logging
-import generate_traffic
-import time
 
-BRAKE_DISTANCE = 20
+
+# 由刹车和距离来判断是否切换视角
 def vehicle_coming(world, walker):
     for actor in world.get_actors().filter("vehicle.*"):
-        actual_distance = math.sqrt((walker.get_location().x - actor.get_location().x)**2 + (walker.get_location().y - actor.get_location().y)**2 + (walker.get_location().z - actor.get_location().z)**2)
-        _yaw = actor.get_transform().rotation.yaw
-        _y = actor.get_location().y
-        w_y = walker.get_location().y
-        _x = actor.get_location().x
-        w_x = walker.get_location().x
-        x_distance = abs(_x-w_x)
-        # 车从左往右走和从右往左
-        if ((_yaw > 80 and _yaw < 100 and _y < w_y) or (_yaw > -100 and _yaw < -80 and _y > w_y) and abs(
-                _y - w_y) < BRAKE_DISTANCE) and actual_distance < 20 and x_distance < 2:
+        actual_distance = math.sqrt((walker.get_location().x - actor.get_location().x) ** 2 + (
+                walker.get_location().y - actor.get_location().y) ** 2 + (
+                                            walker.get_location().z - actor.get_location().z) ** 2)
+        control = actor.get_control()
+
+        if control.brake > 0.8 and actual_distance < 10:
             return True
+
     return False
 
 
@@ -91,10 +77,6 @@ def main():
         type=int,
         help='Port to communicate with TM (default: 8000)')
     argparser.add_argument(
-        '--asynch',
-        action='store_true',
-        help='Activate asynchronous mode execution')
-    argparser.add_argument(
         '--filterw',
         metavar='PATTERN',
         default='walker.pedestrian.*',
@@ -107,48 +89,75 @@ def main():
     args = argparser.parse_args()
     client = carla.Client(args.host, args.port)
     client.set_timeout(10.0)
-    synchronous_master = False
     try:
         world = client.get_world()
-        # # 设置服务端同步
-        # settings = world.get_settings()
-        # settings.synchronous_mode = True
-        # settings.fixed_delta_seconds = 0.02
-        # world.apply_settings(settings)
-        # world.debug.draw_string(carla.Location(x=-6, y=15, z=0.1), str(3),
-        #                         life_time=600, color=carla.Color(255, 0, 0))
-
-        transform = carla.Transform(carla.Location(x=-6, y=25, z=3), carla.Rotation(yaw=0))
         blueprints_Walkers = get_actor_blueprints(world, args.filterw, args.generationw)
+
+        transform = carla.Transform(carla.Location(x=93, y=93, z=0.3), carla.Rotation(yaw=0))
         walker = world.try_spawn_actor(random.choice(blueprints_Walkers), transform)
+        # transform_1 = carla.Transform(carla.Location(x=-88, y=140, z=0.3), carla.Rotation(yaw=0))
+        transform_1 = carla.Transform(carla.Location(x=-57, y=93, z=0.3), carla.Rotation(yaw=0))
+        walker_1 = world.try_spawn_actor(random.choice(blueprints_Walkers), transform_1)
 
-        control = carla.WalkerControl(carla.Vector3D(x=5, y=0, z=0), speed=0.1)
+        # control walker
+        control = carla.WalkerControl(carla.Vector3D(x=5, y=0, z=0), speed=0.5)
         walker.apply_control(control)
+        control_1 = carla.WalkerControl(carla.Vector3D(x=5, y=-15, z=0), speed=0.5)
+        # control_1 = carla.WalkerControl(carla.Vector3D(x=5, y=0, z=0), speed=0.5)
+        walker_1.apply_control(control_1)
 
-        # 显示一次
-        flag = False
+        # [walker, stop, respawn, transform,disappear_distance,sp_lo, speed]
+        spectator_location = carla.Location(x=93, y=75, z=3)
+        spectator_location_1 = carla.Location(x=-57, y=75, z=3)
+        walker_list = [
+              [walker, False, False, transform, 113, spectator_location]
+
+        ]
+        # [walker_1, False, False, transform_1, -38, spectator_location_1]
         while True:
-            if walker.get_location().x > 3:
-                walker.destroy()
-                flag = True
-            # 销毁了重新生成
-            if flag:
-                walker = world.try_spawn_actor(random.choice(blueprints_Walkers), transform)
-                control = carla.WalkerControl(carla.Vector3D(x=5, y=0, z=0), speed=0.1)
-                walker.apply_control(control)
-                flag = False
-            w_x = walker.get_location().x
-            if vehicle_coming(world, walker) and w_x > -2 and w_x <2:
-                # 切换视角至自车辆闯红灯
-                spectator = world.get_spectator()
-                _x = walker.get_location().x
-                _y = walker.get_location().y
-                location = carla.Location(x=_x, y=_y, z=30)
-                spectator_transform = carla.Transform(location, carla.Rotation(pitch=-90))
-                spectator.set_transform(spectator_transform)
+            for walker_list_item in walker_list:
+                # 判断行人前面是否有车，是则停止
+                for actor in world.get_actors().filter("vehicle.*"):
+                    w_x = walker_list_item[0].get_location().x
+                    _x = actor.get_location().x
+                    w_y = walker_list_item[0].get_location().y
+                    _y = actor.get_location().y
+                    wv_distance = math.sqrt((walker_list_item[0].get_location().x - actor.get_location().x) ** 2 + (
+                            walker_list_item[0].get_location().y - actor.get_location().y) ** 2 + (
+                                                        walker_list_item[0].get_location().z - actor.get_location().z) ** 2)
+                    if w_y <= (_y + 2) and w_y >= (_y - 2) and wv_distance < 6 and (_x - w_x) <= 2.5:
+                        walker_list_item[1] = True
+
+                if walker_list_item[1]:
+                    walker_control = walker_list_item[0].get_control()
+                    walker_control.speed = 0
+                    walker_list_item[0].apply_control(walker_control)
+                    walker_list_item[1] = False
+                else:
+                    walker_control = walker_list_item[0].get_control()
+                    walker_control.speed = 0.5
+                    walker_list_item[0].apply_control(control)
+
+                if walker_list_item[0].get_location().x > walker_list_item[4]:
+                    walker_list_item[0].destroy()
+                    walker_list_item[2] = True
+
+                #  respawn walker
+                if walker_list_item[2]:
+                    walker_list_item[0] = world.try_spawn_actor(random.choice(blueprints_Walkers), walker_list_item[3])
+                    control = carla.WalkerControl(carla.Vector3D(x=5, y=0, z=0), speed=0.5)
+                    walker_list_item[0].apply_control(control)
+                    walker_list_item[2] = False
+
+                if vehicle_coming(world, walker_list_item[0]):
+                    spectator = world.get_spectator()
+                    _x = walker_list_item[0].get_location().x
+                    _y = walker_list_item[0].get_location().y
+                    spectator_transform = carla.Transform(walker_list_item[5], carla.Rotation(yaw=80))
+                    spectator.set_transform(spectator_transform)
+
             world.wait_for_tick()
-            # world.tick()
-            time.sleep(0.01)
+
     finally:
 
         # 最后修改为异步模式
